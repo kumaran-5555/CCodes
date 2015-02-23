1e20f60dd790666a9ef7ece4651357099e5969ad
kumaran_5555 <kumaran_5555@outlook.com>
2015-02-23 22:35:51 +0530

    more solved

--- Algos/Trees/RecoverBST.py
+++ Algos/Trees/RecoverBST.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 __author__ = 'kumaran'
 
+class Tree():
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 
 class Solution():
 
 
     def checkLocalMaximaAndMinima(self, output):
+        if len(output) < 2:
+            return
         # everytime a node added based on the current state, see if the previous
         # node forms local maxima or local minima
         if not self.isLocalMaximaFound:
             # find local maxima
-            # prev.prev < prev > curr
-            if len(output) < 3:
-                # we don't have prev.prev
-                if output[-2] > output[-1]:
-                    # found local maxima
-                    self.localMaxima = output[-2]
-                    self.isLocalMaximaFound = True
-            else:
-                if output[-3] < output[-2] and output[-2] > output[-1]:
-                    self.localMaxima = output[-2]
-                    self.isLocalMaximaFound = True
-        else:
-            # find local minima, both maxima and minima can't occur at the same node
-            # hence else
-            # find local maxima
-            # prev.prev < prev > curr
-            if output[-2] > output[-1]:
-                self.localMinima = output[-1]
+            # prev > curr
+            if output[-2].val > output[-1].val:
+                # found local maxima
+                self.localMaxima = output[-2]
+                self.isLocalMaximaFound = True
+        # find local maxima
+        # prev.prev < prev > curr
+        if output[-2].val > output[-1].val:
+            self.localMinima = output[-1]
 
     def inorderTraversalMoris(self, root):
         node = root
@@ -35,7 +34,7 @@ class Solution():
         while node:
             if not node.left:
                 # print self and go right
-                output.append(node.val)
+                output.append(node)
                 self.checkLocalMaximaAndMinima(output)
                 node = node.right
                 continue
@@ -54,7 +53,7 @@ class Solution():
                 # we just came back from the left subtree we
                 # entered earlier, reset the tree
                 temp.right = None
-                output.append(node.val)
+                output.append(node)
                 self.checkLocalMaximaAndMinima(output)
                 node = node.right
         return output
@@ -64,7 +63,35 @@ class Solution():
         self.isLocalMaximaFound = False
         self.localMaxima = None
         self.localMinima = None
+        self.inorderTraversalMoris(root)
+        #print(self.localMaxima, self.localMinima)
+        if not self.localMaxima or not self.localMinima:
+            return
+
+        temp = self.localMaxima.val
+        self.localMaxima.val = self.localMinima.val
+        self.localMinima.val = temp
+        return root
+        '''
+        self.localMaxima = None
+        self.localMinima = None
+        self.inorderTraversalMoris(root)
+        print(self.localMaxima, self.localMinima)
+        '''
 
 if __name__ == '__main__':
+    t = Tree(1)
+    t.left = Tree(3)
+    t.left.right = Tree(4)
+    t.left.left = Tree(2)
+    t.left.left.left = Tree(5)
+    t.right = Tree(7)
+    t.right.left = Tree(6)
+    t.right.right = Tree(8)
+
+    t = Tree(0)
+    t.left = Tree(1)
     s = Solution()
-    
+    s.recoverTree(t)
+
+
