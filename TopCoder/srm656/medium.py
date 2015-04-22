7a99f7b7f85a6c7ef3d387dab521de4dace345a2
kumaran-5555 <kumaran_5555@outlook.com>
2015-04-22 16:08:57 +0530

    modigied

--- TopCoder/srm656/medium.py
+++ TopCoder/srm656/medium.py
@@ -1,37 +1,41 @@
-__author__ = 'serajago'
+
+    class RandomPancakeStackDiv2:
+        def _expected(self, n, numChoosen):
+
+            if self.dpTable[n][numChoosen] != -1:
+                return self.dpTable[n][numChoosen]
+            totalLeftOut = self.n - numChoosen
+            if not totalLeftOut:
+                return 0
+
+            expected = 0
+            #expected = 1/totalLeftOut * (self.n -n - numChoosen)
+
+            for i in range(n):
+                #if choosen[i]:
+                #    continue
+                #choosen[i] = True
+                print(i)
+                expected += (1/totalLeftOut * (self.d[i] + self._expected(i, numChoosen+1)))
+                #choosen[i] = False
+            self.dpTable[n][numChoosen] = expected
+            return expected
 
 
-class RandomPancakeStackDiv2:
-    def _expected(self, n, choosen, delicious):
-        totalLeftOut = sum([1 for i in choosen if not i])
-        if not totalLeftOut:
-            return 0
 
-        expected = 0
-        expected = 1/totalLeftOut * sum([1 for i in range(len(choosen)) if i > n and not choosen[i]]) * delicious
 
-        for i in range(n):
-            if choosen[i]:
-                continue
-            choosen[i] = True
-            expected += (1/totalLeftOut * self._expected(i, choosen, delicious+self.d[i]))
-            choosen[i] = False
+        def expectedDeliciousness(self, d):
+            self.n = len(d)
+            n = len(d)
+            self.dpTable = []
+            for i in range(n+2):
+                self.dpTable.append([-1]*(n+2))
 
-        return expected
+            self.d = [0]+list(d)
+            choosen = [False] * n
+            return  self._expected(n+1, 0)
 
 
 
-
-    def expectedDeliciousness(self, d):
-        n = len(d)
-        self.dpTable = [-1]*n
-        self.d = d
-        choosen = [False] * n
-        return  self._expected(n, choosen, 0)
-
-
-if __name__ == '__main__':
     s =RandomPancakeStackDiv2()
     s.expectedDeliciousness([1,1,1,1,1,1,1,1,1,1])
-
-
