4fd2ce9d5cc97b90694173f810a1700c3eca3f4d
kumaran-5555 <kumaran_5555@outlook.com>
2015-09-22 20:51:16 +0530

    Revert "refactor into vs solution"
    This reverts commit 4376cb2567ccac026cb032718f5ec70131529ec1.

--- CCodes/CCodes/Algos/Trees/node.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-class Node():
-	def __init__(self, val):
-		self.val = val
-		self.left = None
-		self.right = None
-
-
--- CCodes/CCodes/TopCoder/srm656/hard.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'serajago'
-
-
-N=5
-for i in range(1,N+1):
-    for j in range(i+1, N+1):
-        if 
