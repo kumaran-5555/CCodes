104e7257a5b7936ef97fe0175d93704468d8b6ad
Senthilkumaran Rajagopalan <kumaran_5555@outlook.com>
2018-02-20 16:14:46 -0800

    mroe changes

--- Algos2/Algos2/LC/307.py
+++ Algos2/Algos2/LC/307.py
@@ -4,6 +4,7 @@ class Node:
         self.meta = None
         self.left = None
         self.right = None
+        self.mid =  None
 
 
 
@@ -13,21 +14,17 @@ class NumArray:
         """
         :type nums: List[int]
         """
-        self.nums = nums
-        sum = 0
-        self.forward = []
-        self.forward.append(0)
-        for i in nums:
-            sum += i
-            self.forward.append(i)
-
-       self.root = self.buildTree(0, len(nums))
+        self.n = nums                
+        if len(nums) == 0:
+            return
+        self.root = self.buildTree(0, len(nums)-1)
 
     def buildTree(self, start, end):
         if start ==  end:
             n = Node()
             n.idx = (start, end)
-            n.meta = self.forward[start]
+            n.meta = self.n[start]
+            n.mid = start
 
             return n
         
@@ -38,6 +35,7 @@ class NumArray:
         n.left = self.buildTree(start, mid-1)
         n.right = self.buildTree(mid, end)
         n.meta = n.left.meta + n.right.meta
+        n.mid = mid
 
         return n
 
@@ -47,31 +45,34 @@ class NumArray:
         if node is None:
             return
         
-        if node.idx[0] < start or node.idx[1] > end:
+        if node.idx[0] > i or node.idx[1] < i:
             return
 
         node.meta += delta
-        _update(node.left, i, delta)
-        _update(node.right, i, delta)
+        NumArray._update(node.left, i, delta)
+        NumArray._update(node.right, i, delta)
 
     
 
     @staticmethod
-    def _search(node, i):
+    def _search(node, i, j):
+        if i > j:
+            return 0
         if node is None:
             return 0
-        if node.idx[0] > start or node.idx[1] > end:
+        
+        if node.idx[0] >= i and node.idx[1] <= j:            
+            return node.meta
+
+        if node.idx[0] > i and node.idx[0] > j:
             return 0
 
-        if node.idx[0] == i and node.idx[1 == i:
-            return node.meta
-        
-             
-        
+        if node.idx[1] < i and node.idx[1] < j:
+            return 0
 
-            
-        
+        return NumArray._search(node.left, i, j) + NumArray._search(node.right, i, j)
 
+        
     def update(self, i, val):
         """
         :type i: int
@@ -79,6 +80,12 @@ class NumArray:
         :rtype: void
         """
         
+        delta = val - self.n[i]
+        
+        NumArray._update(self.root, i, delta)
+        self.n[i] = val
+
+
 
     def sumRange(self, i, j):
         """
@@ -87,9 +94,19 @@ class NumArray:
         :rtype: int
         """
         
+        return NumArray._search(self.root, i, j)
+
 
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # obj.update(i,val)
 # param_2 = obj.sumRange(i,j)
+
+if __name__ == '__main__':
+    
+    c = NumArray([7,2,7,2,0])
+    print(c.update(4, 6))
+    print(c.sumRange(0,2))
+    print(c.update(1, 2))
+    print(c.sumRange(0,2))
